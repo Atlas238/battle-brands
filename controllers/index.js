dccedf81b392fa52bb48798f0fa9ab559ffc1046
Derek DeLizo <delizoderek@gmail.com>
2021-08-03 13:00:20 -0700

    Updated userRoutes for logging in

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const apiRoutes = require('./apiRoutes');
 
-// router.use("/user",userRoutes);
-// router.use("/api",apiRoutes);
+router.use("/user",userRoutes);
+router.use("/api",apiRoutes);
 
 router.get('/', async (req, res) => {
     res.render("homepage",{
--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,16 +1,62 @@
-const userRouter = require('express').Router();
+const router = require("express").Router();
+const User = require("../models/User");
 
-userRouter.get('/',(req,res) => {
-    res.send('Hello from your server!');
+router.post("/", async (req, res) => {
+    console.log(req.body);
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+      console.log(err);
+    res.status(400).json(err);
+  }
 });
 
-userRouter.post('/',(req,res) => {
-    console.log('req.baseurl');
-    res.send(`${req.method} request received`);
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-userRouter.delete('/',(req,res) => {
-    res.send('Message from delete');
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
-module.exports = userRouter;
+module.exports = router;
