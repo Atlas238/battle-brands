e5a4096a642fb4e2757ea477bd574283239c2a57
Atlas <jack@bentsen.us>
2021-08-03 12:56:59 -0700

    getting rid of my edits...

--- controllers/index.js
+++ controllers/index.js
@@ -2,21 +2,8 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const apiRoutes = require('./apiRoutes');
 
-// router.use("/user",userRoutes);
-// router.use("/api",apiRoutes);
-
 router.get('/', async (req, res) => {
-    res.render("homepage",{
-        name:"Derek",
-    });
+    res.render("homepage");
   });
-
-const apiRoutes = require('./api');
-const homeRoutes = require('./home');
-const authRoutes = require('./passport/passport-routes');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-router.use('/auth/', authRoutes);
 
 module.exports = router;
