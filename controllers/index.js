c51f7717f72b1d2e7d3a5013cb70d84975fe7af0
Atlas <jack@bentsen.us>
2021-08-03 11:33:36 -0700

    local changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
 
+const apiRoutes = require('./api');
+const homeRoutes = require('./home');
+const authRoutes = require('./passport/passport-routes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/auth/', authRoutes);
+
 module.exports = router;
