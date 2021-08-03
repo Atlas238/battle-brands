af7b279bfe333cd61c3afd990f6afbf3650f1999
Derek DeLizo <delizoderek@gmail.com>
2021-08-02 18:23:24 -0700

    Added routes for user and api calls

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const apiRoutes = require('./apiRoutes');
+
+router.use("/user",userRoutes);
+router.use("/api",apiRoutes);
 
 module.exports = router;
