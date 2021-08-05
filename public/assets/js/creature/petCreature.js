f492459fa210dda1824695ff2a5fd00f0b9024dc
Atlas <jack@bentsen.us>
2021-08-04 20:45:30 -0700

    basic pet feed and catch scripts

--- public/assets/js/creature/petCreature.js
+++ public/assets/js/creature/petCreature.js
@@ -1,9 +1,32 @@
-// CAP OF 4 for happiness
-// clean/care/pet DO increment grooming/ + happiness
-// API /API/STATS/:ID
-const petCreature = async (object) => {
+const petCreature = async () => {
+    try {
+        fetch('http://localhost:3001/api/stats/1', {
+            method: 'GET'
+        }).then((response) => {
+            response.json();
+        }).then((data) => {
 
-    const userCreatureData = await fetch(`/api/stats/${object.user_id}`);
-    
-    
-}
+            // Now have carestat obj...
+            
+            if (data[0].carestat.energy > 0) {
+                let updatedGroomingVal = data[0].carestat.grooming++;
+                let updatedEnergyVal = data[0].carestat.energy--;
+                try {
+                    fetch('http://localhost:3001/api/stats/1', {
+                        method: 'PUT',
+                        body: {
+                            carestat: {
+                                grooming: updatedGroomingVal,
+                                energy: updatedEnergyVal,
+                            },
+                        },
+                    }).then((response) => {
+                        response.json();
+                    }).then((data) => console.log(data));
+                } catch (error) { console.log(error) };
+            };
+        });
+    } catch (error) { console.log(error) };
+};
+
+module.exports = petCreature;
