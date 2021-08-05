e2aa0b67f0704ef6bed88ad47fa5dbfc54b2e41b
David Lucio <david.a.lucio@gmail.com>
2021-08-04 21:15:10 -0700

    Resolved conflicts locally.

--- controllers/index.js
+++ controllers/index.js
@@ -2,16 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const apiRoutes = require('./api');
+const authRoutes = require('./authRoutes');
 const frontEndRoutes = require('./frontEndRoutes');
-
-// const authRoutes = require('./authRoutes');
-// const homeRoutes = require('./homeRoutes');
-// const { route } = require('./authRoutes');
+const homeRoutes = require('./homeRoutes');
+const { route } = require('./authRoutes');
 
 router.use("/api",apiRoutes);
+router.use("/user",authRoutes);
 router.use(frontEndRoutes);
-
-// router.use("/user",authRoutes);
-//router.use(homeRoutes);
+router.use(homeRoutes);
 
 module.exports = router;
