5980f5b213a5f90d59726fc0fdd85751f0fb6f22
Atlas <jack@bentsen.us>
2021-08-05 12:57:28 -0700

    passport fixes again

--- controllers/passport/passportRoutes.js
+++ controllers/passport/passportRoutes.js
@@ -17,13 +17,15 @@ function(accessToken, refreshToken, profile, done) {
 // LINKEDIN SETUP
 const LinkedInStrategy = passportLinkedIn.Strategy;
 passport.use(new LinkedInStrategy({
-    consumerKey: process.env.LINKEDIN_API_KEY,
-    consumerSecret: process.env.LINKEDIN_SECRET_KEY,
-    callbackURL: "http://localhost:3001/passport/auth/linkedin/callback"
-}, function(token, tokenSecret, profile, done) {
-      return done(err, profile);
-}
-));
+    clientID: process.env.LINKEDIN_API_KEY,
+    clientSecret: process.env.LINKEDIN_SECRET_KEY,
+    callbackURL: "http://localhost:3001/passport/auth/linkedin/callback",
+    state: true
+}, function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+        return done(err, profile);
+    });
+}));
 
 // User Serialize Setps...
 passport.serializeUser(function(user, done) {
@@ -33,13 +35,17 @@ passport.deserializeUser(function(user, done) {
     done(null, user);
 });
 
-// FB Route (Following example format from docs --- they store FB auth data for login purposes, dont necessarily need to add anything to db and then wouldnt need federated route)
+// Facebook Auth Routes - first route sends to FB for login
 socialLinkRouter.get('/passport/auth/facebook', passport.authenticate('facebook'));
+// Callback route for when user returns
 socialLinkRouter.get('/passport/auth/facebook/callback',
   passport.authenticate('facebook', { successMessage:'IT WORKS!', failureMessage: 'IT DOESNT WORK!'}));
 
 // // Linkedin Route (also docs)
-socialLinkRouter.get('/passport/auth/linkedin', passport.authenticate('linkedin'));
+socialLinkRouter.get('/passport/auth/linkedin', passport.authenticate('linkedin'), function(req, res){
+    // Redirected to LinkedIn for auth, function not called
+});
+// Callback route for when user returns
 socialLinkRouter.get('/passport/auth/linkedin/callback',
   passport.authenticate('linkedin', { successMessage: 'IT WORKS!', failureMessage: 'IT DOESNT WORK!' }));
 
