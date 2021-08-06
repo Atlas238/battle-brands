f28d841e73030749fceecb342bee107f9477caf6
Atlas <jack@bentsen.us>
2021-08-06 12:57:58 -0700

    pushing scripts with new routes

--- public/assets/js/creature/updateCreature.js
+++ public/assets/js/creature/updateCreature.js
@@ -5,12 +5,14 @@ const numHoursGrooming = 5;
 const numHoursEnergy = 1;
 
 const updateCreatureId = document.getElementById('icon').getAttribute('data-id')
+const userId = document.getElementById('icon').getAttribute('data-user');
+const icon = document.getElementById('icon');
+const iconDiv = document.getElementById('icon-holder');
 
-const updateCreatureStats = (id) => {
+const updateCreatureStats = async (creatureID, userID) => {
     // Get our data from the server
     try {
-        fetch(`http://localhost:3001/creature/care/${id}`, { method: 'GET' }).then((response) => response.json())
-        .then((currentCare) => {
+        const currentCare = await fetch(`http://localhost:3001/creature/care/${creatureID}`, { method: 'GET' }).then((response) => response.json());
 
             console.log(currentCare);
             const dbTime = new Date(Date.parse(currentCare.lastinteraction)).getTime();
@@ -49,7 +51,7 @@ const updateCreatureStats = (id) => {
             };
             try {
                 // Post request to update creature stats
-                fetch(`http://localhost:3001/creature/care/${id}`, {
+                fetch(`http://localhost:3001/creature/care/${creatureID}`, {
                     method: 'PUT',
                     body: updatedCareStat
                 });
@@ -57,7 +59,48 @@ const updateCreatureStats = (id) => {
                 console.log(error);
                 console.log('Error when updating stats to database');
             };
-        });
+        
+            // ANIMATIONS...
+            try {
+            const newPull = await fetch(`http://localhost:3001/creature/care/${creatureID}`, {
+                method: 'GET',
+            });
+            console.log(newPull.json());
+            const brandPull = await fetch(`http://localhost:3001/creature/?user=${userId}&creature=${creatureID}`, {
+                method: 'GET',
+            });
+            console.log(brandPull.json());
+            if (newPull.happiness > 3) {
+                icon.setAttribute('class', `fab ${brandPull[0].brand.icon} fa-8x`);
+                iconDiv.setAttribute('class', '');
+                // Might need to move this statement to AFTER the animation class add (eg: happy)
+                void icon.offsetWidth;
+                icon.setAttribute('class', `fab ${brandPull[0].brand.icon} fa-8x happy`);
+            } else if (newPull.hunger > 4) {
+                icon.setAttribute('class', `fab ${brandPull[0].brand.icon} fa-8x`);
+                iconDiv.setAttribute('class', '');
+                void icon.offsetWidth;
+                icon.setAttribute('class', `fab ${brandPull[0].brand.icon} fa-8x hungry`);
+
+                // COMMENTING OUT GROOMING FOR NOW (COMPLICATED ANIMATION CURRENTLY)
+
+                // } else if (newPull.grooming = 5) {
+                //     icon.setAttribute('class', `fab ${brandPull[0].brand.icon} fa-8x`);
+                //     iconDiv.setAttribute('class', '');
+                //     void icon.offsetWidth;
+                //     icon.setAttribute('class', `fab ${brandPull[0].brand.icon} fa-8x groomed`);
+                //     iconDiv.setAttribute('class', 'creature-groomed');
+
+            } else {
+                icon.setAttribute('class', `fab ${brandPull[0].brand.icon} fa-8x`);
+                iconDiv.setAttribute('class', '');
+                void icon.offsetWidth;
+                icon.setAttribute('class', `fab ${brandPull[0].brand.icon} fa-8x rest`);
+            }
+        } catch (error) {
+            console.log(error);
+            console.log('Error setting animations');
+        };
     } catch (error) {
         console.log(error);
         console.log('Error getting data from database');
