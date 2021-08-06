4973fa6f0de6c5939098d0ec01e1c59b30586a07
Atlas <jack@bentsen.us>
2021-08-06 14:41:02 -0700

    commenting out

--- controllers/passport/passportRoutes.js
+++ controllers/passport/passportRoutes.js
@@ -51,47 +51,54 @@ passport.use(new LinkedInStrategy({
 // ROUTES
 // Facebook Routes
 socialLinkRouter.get('/passport/auth/facebook', passport.authenticate('facebook'));
-socialLinkRouter.get('https:/battle-brands.herokuapp.com/passport/auth/facebook/callback',
+socialLinkRouter.get('/passport/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/create'}), (req, res) => {
+      console.log(req);
+      console.log(res);
     //   CREATE FACEBOOK CREATURE FOR USER WITH GIVEN ID
-    try {
-        if (req.session.user_id) {
-            let newCreature = {
-                user_id: req.session.user_id,
-                name: `newPet${req.session.user_id}`,
-                // FB BrandID
-                brand_id: 2,
-                type_id: 2,
-                combatstat_id: 3,
-                carestat_id: 1,
-                exp: 0,
-                health: 10,
-                // Isnt this just brand_id again?
-                brand: 2,
-                type: 2,
-                // Isnt this just care/combat id again?
-                care_stat: 1,
-                combat_stat: 3
-            };
-            fetch('http://battle-brands.heroukapp.com/creature/create', {
-                method: 'POST',
-                body: newCreature
-            }).then((response) => {
-                response.json();
-            }).then((data) => {
-                console.log(data);
-                res.redirect('/profile');
-            });
-        };
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
-    };
+    // try {
+    //     if (req.session.user_id) {
+    //         let newCreature = {
+    //             user_id: req.session.user_id,
+    //             name: `newPet${req.session.user_id}`,
+    //             // FB BrandID
+    //             brand_id: 2,
+    //             type_id: 2,
+    //             combatstat_id: 3,
+    //             carestat_id: 1,
+    //             exp: 0,
+    //             health: 10,
+    //             // Isnt this just brand_id again?
+    //             brand: 2,
+    //             type: 2,
+    //             // Isnt this just care/combat id again?
+    //             care_stat: 1,
+    //             combat_stat: 3
+    //         };
+    //         try {
+    //             fetch('http://battle-brands.heroukapp.com/creature/create', {
+    //                 method: 'POST',
+    //                 body: newCreature
+    //             }).then((response) => {
+    //                 response.json();
+    //             }).then((data) => {
+    //                 console.log(data);
+    //                 res.redirect('/profile');
+    //             });
+    //         } catch (error) {
+    //             res.status(500).json(error);
+    //             console.log(error);
+    //         }
+    //     };
+    // } catch (error) {
+    //     console.log(error);
+    //     res.status(500).json(error);
+    // };
   });
 
 // // Linkedin Route
 socialLinkRouter.get('/passport/auth/linkedin', passport.authenticate('linkedin'));
-socialLinkRouter.get('https:/battle-brands.herokuapp.com/passport/auth/linkedin/callback',
+socialLinkRouter.get('/passport/auth/linkedin/callback',
   passport.authenticate('linkedin', { failureRedirect: '/login' }), (req, res) => {
     // CREATE LINKEDIN CREATURE FOR USER WITH GIVEN ID
     try {
