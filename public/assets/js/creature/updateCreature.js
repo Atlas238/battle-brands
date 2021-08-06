296403f51070af160eb9e4632ade2528854981e0
Atlas <jack@bentsen.us>
2021-08-06 12:10:15 -0700

    update Creature works! changes stats based on time

--- public/assets/js/creature/updateCreature.js
+++ public/assets/js/creature/updateCreature.js
@@ -6,49 +6,62 @@ const numHoursEnergy = 1;
 
 const updateCreatureId = document.getElementById('icon').getAttribute('data-id')
 
-// TODO: change to carestat routes
 const updateCreatureStats = (id) => {
     // Get our data from the server
     try {
-        fetch(`http://localhost:3001/creature/${id}`, { method: 'GET' })
-        .then((response) => response.json())
-        .then((data) => {
-            const dbTime = new Date(Date.parse(data[0].carestat.lastinteraction)).getTime();
+        fetch(`http://localhost:3001/creature/care/${id}`, { method: 'GET' }).then((response) => response.json())
+        .then((currentCare) => {
+
+            console.log(currentCare);
+            const dbTime = new Date(Date.parse(currentCare.lastinteraction)).getTime();
             let currentTime = new Date().getTime();
+
             // How long since last visit in hours?
             const diffInTime = Math.round((currentTime - dbTime) / (1000 * 60 * 60));
+
             // Hunger...
             const updatedCareStat = { 
-                id: data[0].carestat.id,
+                id: currentCare.id,
                 lastinteraction: new Date(),
             };
+
             // Check and degrade values...
             // Dete
             if (diffInTime > numHoursHunger) {
-                updatedCareStat.hunger = data[0].carestat.hunger--;
+                if(currentCare.hunger > 0) {
+                    updatedCareStat.hunger = currentCare.hunger--;
+                };
             };
             if (diffInTime > numHoursHappiness) {
-                updatedCareStat.happiness = data[0].carestat.happiness--;
+                if(currentCare.happiness > 0) {
+                    updatedCareStat.happiness = currentCare.happiness--;
+                };
             };
             if (diffInTime > numHoursGrooming) {
-                updatedCareStat.grooming = data[0].carestat.grooming--;
+                if(currentCare.grooming > 0) {
+                    updatedCareStat.grooming = currentCare.grooming--;
+                };
             };
             if (diffInTime > numHoursEnergy) {
-                updatedCareStat.energy = data[0].carestat.energy++;
+                if (currentCare.energy < 5) {
+                    updatedCareStat.energy = currentCare.energy++;
+                };
             };
             try {
                 // Post request to update creature stats
-                fetch(`http://localhost:3001/api/stats/${id}`, {
+                fetch(`http://localhost:3001/creature/care/${id}`, {
                     method: 'PUT',
                     body: updatedCareStat
                 });
             } catch (error) {
+                console.log(error);
                 console.log('Error when updating stats to database');
             };
         });
     } catch (error) {
+        console.log(error);
         console.log('Error getting data from database');
     };
 }
 
-// updateCreatureStats(updateCreatureId);
+updateCreatureStats(updateCreatureId);
