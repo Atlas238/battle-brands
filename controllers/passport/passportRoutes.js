3b28e36c5fc47887f3b4b4cfb932b90b78a23895
Atlas <jack@bentsen.us>
2021-08-05 21:53:31 -0700

    twitter changes

--- controllers/passport/passportRoutes.js
+++ controllers/passport/passportRoutes.js
@@ -10,7 +10,7 @@ const FacebookStrategy = passportFacebook.Strategy;
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://battle-brands.herokuapp.com/passport/auth/facebook/callback"
+    callbackURL: "https://battle-brands.herokuapp.com/passport/auth/facebook/callback"
 },
 function(accessToken, refreshToken, profile, done) {
     return done(null, profile);
@@ -20,7 +20,7 @@ const LinkedInStrategy = passportLinkedIn.Strategy;
 passport.use(new LinkedInStrategy({
     clientID: process.env.LINKEDIN_API_KEY,
     clientSecret: process.env.LINKEDIN_SECRET_KEY,
-    callbackURL: "http://battle-brands.herokuapp.com/passport/auth/linkedin/callback",
+    callbackURL: "https://battle-brands.herokuapp.com/passport/auth/linkedin/callback",
     scope: ['r_emailaddress', 'r_liteprofile'],
     state: true
 }, function(accessToken, refreshToken, profile, done) {
@@ -32,8 +32,8 @@ passport.use(new LinkedInStrategy({
 const TwitterStrategy = passportTwitter.Strategy;
 passport.use(new TwitterStrategy({
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
-    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://battle-brands.herokuapp.com/passport/auth/twitter/callback"
+    consumerSecret: process.env.TWITTER_CONSUMER_SERCRET,
+    callbackURL: "https://battle-brands.herokuapp.com/passport/auth/twitter/callback"
 },
 function(token, tokenSecret, profile, cb) {
     return cb(null, profile);
@@ -48,7 +48,6 @@ passport.deserializeUser(function(user, done) {
 });
 
 // ROUTES
-
 // Facebook Routes
 socialLinkRouter.get('/passport/auth/facebook', passport.authenticate('facebook'));
 // Callback
