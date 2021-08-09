77275a43c2347c1396905e4bf903931a3061a650
Derek DeLizo <delizoderek@gmail.com>
2021-08-09 11:23:02 -0700

    Click functionality working

--- public/assets/js/createCreature.js
+++ public/assets/js/createCreature.js
@@ -1,9 +1,12 @@
 const creatureBox = document.querySelector('#creature-selector');
 
 const makeACreature = (event) => {
-    // event.preventDefault();
-    const card = event.target;
-    // console.log(card);
+    event.preventDefault();
+    let card = event.target;
+    if(card.id === 'icon'){
+        card = card.parentElement;
+    }
+
     if(card.dataset.type && card.dataset.brand){
         console.log(card.dataset.type);
         console.log(card.dataset.brand);
