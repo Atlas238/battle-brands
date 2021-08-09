a81bfe8917ab7d92ca50d41a9ff6954cc7126bcd
Derek DeLizo <delizoderek@gmail.com>
2021-08-09 10:57:49 -0700

    Removed depreciation warning and added event listeners to create page

--- public/assets/js/creature/updateCreature.js
+++ public/assets/js/creature/updateCreature.js
@@ -19,12 +19,12 @@ const host = 'http://localhost:3001';
 let notSyncing = true; //Is set to false when the player interacts with their creature, so the autosync will not run
 
 const currentCreature = {
-  id: 0,
-  hunger: 0,
-  happiness: 0,
-  grooming: 0,
-  energy: 0,
-  lastinteraction: moment(),
+    id: updateCreatureId,
+    hunger: 0,
+    happiness: 0,
+    grooming: 0,
+    energy: 0,
+    lastinteraction: moment(),
 };
 
 
@@ -34,8 +34,7 @@ const scanStats = () => {
     currentCreature.hunger = Number(document.getElementById('hungerState').getAttribute('value'));
     currentCreature.grooming = Number(document.getElementById('groomingState').getAttribute('value'));
     currentCreature.energy = Number(document.getElementById('energyState').getAttribute('value'));
-    currentCreature.lastinteraction = moment(document.getElementById('icon').getAttribute('data-action')).format('YYYY-MM-DDTHH:mm:ss');
-    console.log(currentCreature);
+    currentCreature.lastinteraction = moment(Date.parse(document.getElementById('icon').getAttribute('data-action'))).format('YYYY-MM-DDTHH:mm:ss');
 }
 
 
@@ -51,45 +50,45 @@ const init = async () => {
 
 
 const adjustCreatureStats = () => {
-  const dbTime = moment(currentCreature.lastinteraction);
-  let currentTime = moment();
-  let diffInTime = currentTime.diff(dbTime, "hours");
-  diffInTime = diffInTime < 0 ? 0 : diffInTime;
+    const dbTime = moment(currentCreature.lastinteraction);
+    let currentTime = moment();
+    let diffInTime = currentTime.diff(dbTime,'hours');
+    diffInTime = diffInTime < 0 ? 0 : diffInTime;
 
-  // Check and degrade values...
-  // Delete
-  if (diffInTime > numHoursHunger) {
-    const valueDecrease = Math.floor(diffInTime / numHoursHunger);
-    if (currentCreature.hunger - valueDecrease > 0) {
-      currentCreature.hunger -= valueDecrease;
-    } else {
-      currentCreature.hunger = 0;
+    // Check and degrade values...
+    // Delete
+    if (diffInTime > numHoursHunger) {
+        const valueDecrease = Math.floor(diffInTime/numHoursHunger);
+        if(currentCreature.hunger - valueDecrease > 0) {
+            currentCreature.hunger -= valueDecrease;
+        } else {
+            currentCreature.hunger = 0;
+        }
     }
-  }
-  if (diffInTime > numHoursHappiness) {
-    const valueDecrease = Math.floor(diffInTime / numHoursHappiness);
-    if (currentCreature.happiness - valueDecrease > 0) {
-      currentCreature.happiness -= valueDecrease;
-    } else {
-      currentCreature.happiness = 0;
+    if (diffInTime > numHoursHappiness) {
+        const valueDecrease = Math.floor(diffInTime/numHoursHappiness);
+        if(currentCreature.happiness - valueDecrease > 0) {
+            currentCreature.happiness -= valueDecrease;
+        } else {
+            currentCreature.happiness = 0;
+        }
     }
-  }
-  if (diffInTime > numHoursGrooming) {
-    const valueDecrease = Math.floor(diffInTime / numHoursGrooming);
-    if (currentCreature.grooming - valueDecrease > 0) {
-      currentCreature.grooming -= valueDecrease;
-    } else {
-      currentCreature.grooming = 0;
+    if (diffInTime > numHoursGrooming) {
+        const valueDecrease = Math.floor(diffInTime/numHoursGrooming);
+        if(currentCreature.grooming - valueDecrease > 0) {
+            currentCreature.grooming -= valueDecrease;
+        } else {
+            currentCreature.grooming = 0;
+        }
     }
-  }
-  if (diffInTime > numHoursEnergy) {
-    const valueDecrease = Math.floor(diffInTime / numHoursEnergy);
-    if (currentCreature.energy - valueDecrease > 0) {
-      currentCreature.energy -= valueDecrease;
-    } else {
-      currentCreature.energy = 0;
+    if (diffInTime > numHoursEnergy) {
+        const valueDecrease = Math.floor(diffInTime/numHoursEnergy);
+        if(currentCreature.energy - valueDecrease > 0) {
+            currentCreature.energy -= valueDecrease;
+        } else {
+            currentCreature.energy = 0;
+        }
     }
-  }
 };
 
 
@@ -104,13 +103,13 @@ const updateStats = async () => {
 
 // Database gets updated every minute
 const startDbAutoSync = () => {
-  const syncInterval = setInterval(() => {
-    if (notSyncing) {
-      console.log("Auto Sync");
-      await updateDatabase();
-    }
-  }, 1000);
-};
+    const syncInterval = setInterval(() => {
+        if(notSyncing){
+            console.log("Auto Sync");
+            updateDatabase();
+        }
+    },1000);
+}
 
 
 const renderMeters = (progObj,currValue) => {
@@ -145,35 +144,18 @@ const updateDatabase = async () => {
             });
             const respData = await updateSql.json();
 
-const updateDatabase = async () => {
-  // If id is 0 then the creature was not loaded properly
-  if (currentCreature.id !== 0) {
-    console.log("Syncing now");
-    try {
-      const updateSql = await fetch(
-        `${host}/creature/care/${updateCreatureId}`,
-        {
-          method: "PUT",
-          body: JSON.stringify(currentCreature),
-          headers: {
-            "Content-Type": "application/json",
-          },
+            if(respData.message){
+                console.log("Data backed up on the super secret cloud");
+            } else {
+                console.log("Data not backed up... :'(");
+            }
+        } catch (error){
+            console.log(error);
         }
-      );
-      const respData = await updateSql.json();
-
-      if (respData.message) {
-        console.log("Data backed up on the super secret cloud");
-      } else {
-        console.log("Data not backed up... :'(");
-      }
-    } catch (error) {
-      console.log(error);
+    } else {
+        // clearInterval(syncInterval);
+        console.log(currentCreature.id);
     }
-  } else {
-    // clearInterval(syncInterval);
-    console.log(currentCreature.id);
-  }
 };
 
 
@@ -191,7 +173,7 @@ feedBtn.addEventListener('click', (event) => {
             console.log('Creature looks more energetic!')
             adjustEnergy(currentCreature.energy);
         }
-
+        
         currentCreature.lastinteraction=moment().format('YYYY-MM-DDTHH:mm:ss');
         updateDatabase();
     }
